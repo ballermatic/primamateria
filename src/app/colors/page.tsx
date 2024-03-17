1e4951e334472a32f77ae0115d9f055f4a6762bd
Neil Baller <neil@gravybo.at>
2024-03-17 10:37:22 -0500

    Color selects page stub

---
+++ src/app/colors/page.tsx
@@ -0,0 +1,7 @@
+export default function ColorsPage() {
+  return (
+    <main className='bg-neutral-500 flex min-h-screen flex-col items-center justify-between p-6'>
+      <h1 className='font-bold text-4xl'>Color Selects</h1>
+    </main>
+  );
+}
