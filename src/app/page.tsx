3308f8f94ec5f1be13c99475322498ce19a87cf5
Neil Baller <neil@gravybo.at>
2024-03-17 18:26:13 -0500

    Demo styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,14 @@ export default function HomePage() {
     <div
       className='bg-cover bg-center bg-fixed min-h-screen'
       style={{ backgroundImage: 'url(gh-art/hp-bg.webp)', zIndex: -1 }}>
-      <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-        <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
-          <p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
-            Non Euclidian geometries abound
-          </p>
-          <div className='fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none'>
+      <main className='text-white flex min-h-screen flex-col items-center justify-between p-24'>
+        <div className='z-10 max-w-5xl w-full items-center justify-between text-sm lg:flex'>
+          <a
+            className='fixed left-0 top-0 flex w-full justify-center border-chimney-500 hover:border-chimney-100 pb-6 pt-8 backdrop-blur-sm hover:backdrop-blur-xl from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:p-4 shadow-2xl shadow-smoke-500'
+            href='/colors'>
+            Non Euclidian Color Selects
+          </a>
+          <div className='fixed bottom-0 left-0 flex h-48 w-full items-end justify-center lg:static lg:h-auto lg:w-auto lg:bg-none'>
             <a
               className='pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0'
               href='https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
@@ -20,7 +22,7 @@ export default function HomePage() {
               <Image
                 src='/vercel.svg'
                 alt='Vercel Logo'
-                className='dark:invert'
+                className='invert'
                 width={100}
                 height={24}
                 priority
@@ -29,7 +31,7 @@ export default function HomePage() {
           </div>
         </div>
 
-        <div className="relative z-10 flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial from-white to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic from-white to-transparent after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br from-white to-transparent before:dark:opacity-10 after:dark:from-white after:dark:to-transparent after:dark:opacity-40 before:lg:h-[360px]">
+        <div className='relative z-10 flex place-items-center'>
           <Image
             className='relative'
             src='/gh-art/eldritchfoundation.webp'
@@ -40,10 +42,10 @@ export default function HomePage() {
           />
         </div>
 
-        <div className='mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left'>
+        <div className='mb-32 grid gap-4 text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left'>
           <a
             href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-            className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-500 hover:dark:bg-stone-800/60'
+            className='group rounded-lg border border-chimney-500 hover:border-chimney-50 px-5 py-4 transition-colors backdrop-blur-sm hover:backdrop-blur-xl shadow-2xl shadow-smoke-500'
             target='_blank'
             rel='noopener noreferrer'>
             <h2 className={`mb-3 text-2xl font-semibold`}>
@@ -60,7 +62,7 @@ export default function HomePage() {
 
           <a
             href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-            className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-500 hover:dark:bg-stone-800/60'
+            className='group rounded-lg border border-chimney-500 hover:border-chimney-50 px-5 py-4 transition-colors backdrop-blur-sm hover:backdrop-blur-xl shadow-2xl shadow-smoke-500'
             target='_blank'
             rel='noopener noreferrer'>
             <h2 className={`mb-3 text-2xl font-semibold`}>
@@ -77,7 +79,7 @@ export default function HomePage() {
 
           <a
             href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-            className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-500 hover:dark:bg-stone-800/60'
+            className='group rounded-lg border border-chimney-500 hover:border-chimney-50 px-5 py-4 transition-colors backdrop-blur-sm hover:backdrop-blur-xl shadow-2xl shadow-smoke-500'
             target='_blank'
             rel='noopener noreferrer'>
             <h2 className={`mb-3 text-2xl font-semibold`}>
@@ -94,7 +96,7 @@ export default function HomePage() {
 
           <a
             href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-            className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-500 hover:dark:bg-stone-800/60'
+            className='group rounded-lg border border-chimney-500 hover:border-chimney-50 px-5 py-4 transition-colors backdrop-blur-sm hover:backdrop-blur-xl shadow-2xl shadow-smoke-500'
             target='_blank'
             rel='noopener noreferrer'>
             <h2 className={`mb-3 text-2xl font-semibold`}>
