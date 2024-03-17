674a19aa2ad3221eb89806eaf646038aa15dfb57
Neil Baller <neil@gravybo.at>
2024-03-17 15:24:11 -0500

    Color selects page

---
+++ src/app/colors/ColorCard.tsx
@@ -0,0 +1,22 @@
+// ColorCard.tsx
+type ColorCardProps = {
+  colorName: string; // Make sure you pass the color name to this component
+  colorShades: Record<string, string>;
+};
+
+const ColorCard: React.FC<ColorCardProps> = ({ colorName, colorShades }) => {
+  return (
+    <div className='flex flex-wrap sm:flex-nowrap'>
+      {Object.entries(colorShades).map(([shade, _]) => (
+        <div
+          key={shade}
+          className='flex-auto'>
+          <div className={`h-24 mb-1 bg-${colorName}-${shade}`}></div>
+          <p className='text-sm'>{shade}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ColorCard;
--- src/app/colors/page.tsx
+++ src/app/colors/page.tsx
@@ -1,7 +1,23 @@
+import ColorCard from './ColorCard';
+import colorsData from './colors.json';
+
 export default function ColorsPage() {
+  const { colors } = colorsData;
+
   return (
-    <main className='bg-neutral-500 flex min-h-screen flex-col items-center justify-between p-6'>
+    <main className='flex min-h-screen flex-col items-center justify-between p-6 text-smoke-500'>
       <h1 className='font-bold text-4xl'>Color Selects</h1>
+      <div className='container mx-auto p-4'>
+        {Object.entries(colors).map(([colorName, colorShades]) => (
+          <div key={colorName}>
+            <h2 className='text-xl font-bold mt-6 mb-2'>{colorName}</h2>
+            <ColorCard
+              colorName={colorName}
+              colorShades={colorShades}
+            />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
