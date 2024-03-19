c4b6e7891fe31c2cb4118312dd7222d3422b46d4
Neil Baller <neil@gravybo.at>
2024-03-19 18:21:58 -0500

    Typography body text specimen

--- src/app/typography/page.tsx
+++ src/app/typography/page.tsx
@@ -1,44 +1,78 @@
 export default function TypographyPage() {
   return (
-    <main className='flex min-h-screen flex-col items-center ~p-3/8 text-smoke-500'>
-      <h1 className='font-bold text-4xl'>Typography</h1>
-      <div className='container mx-auto'>
-        <div className='pt-12'>
-          <p className='exa'>Wandering Quails Roam Near Eucalyptus</p>
-          <p className='mt-8 pt-2 border-t border-gray-300 tracking-wide'>
-            <span className='nano caps text-gray-500'>classname:</span> exa
+    <main className='container mx-auto flex min-h-screen flex-col ~p-3/8 text-smoke-500'>
+      <h1 className='font-bold text-4xl pb-12'>Typography</h1>
+      <p className='mt-8 pb-2 border-b-2 border-smoke-500 font-bold'>Headings</p>
+      <div className='pt-12'>
+        <p className='exa'>Wandering Quails Roam Near Eucalyptus</p>
+        <p className='mt-8 pt-2 border-t border-gray-300 tracking-wide'>
+          <span className='nano caps text-gray-500'>classname:</span> exa
+        </p>
+      </div>
+      <div className='pt-12'>
+        <p className='peta'>Mythical xylophones glisten</p>
+        <p className='mt-8 pt-2 border-t border-gray-300 tracking-wide'>
+          <span className='nano caps text-gray-500'>classname:</span> peta
+        </p>
+      </div>
+      <div className='pt-12'>
+        <p className='tera'>Kingly sparrows quiz the evening wind</p>
+        <p className='mt-8 pt-2 border-t border-gray-300 tracking-wide'>
+          <span className='nano caps text-gray-500'>classname:</span> tera
+        </p>
+      </div>
+      <div className='pt-12'>
+        <p className='giga'>Brisk ferns envelop damp pathways</p>
+        <p className='mt-8 pt-2 border-t border-gray-300 tracking-wide'>
+          <span className='nano caps text-gray-500'>classname:</span> giga
+        </p>
+      </div>
+      <div className='pt-12'>
+        <p className='mega'>Gargantuan whelks explore neptunian depths</p>
+        <p className='mt-8 pt-2 border-t border-gray-300 tracking-wide'>
+          <span className='nano caps text-gray-500'>classname:</span> mega
+        </p>
+      </div>
+      <div className='pt-12'>
+        <p className='kilo'>Vibrant, quirky jackals roam zeniths, eclipsing moonlit fjords</p>
+        <p className='mt-8 pt-2 border-t border-gray-300 tracking-wide'>
+          <span className='nano caps text-gray-500'>classname:</span> kilo
+        </p>
+      </div>
+      <p className='mt-24 pb-2 border-b-2 border-smoke-500 font-bold'>Body Text</p>
+      <div className='pt-12'>
+        <div className='lede'>
+          <p>
+            In a verdant glen, quirky zebras whispered enigmatic tales under the moon’s glow.
+            Juxtaposing realms, cryptic foxes danced by the sapphire lakes, their shadows entwining
+            with twilight’s embrace.
+          </p>
+          <p>
+            Nearby, whimsical jays orchestrated a symphony, echoing through the serene canopy of the
+            enchanted forest.
           </p>
         </div>
-        <div className='pt-12'>
-          <p className='peta'>Mythical xylophones glisten</p>
-          <p className='mt-8 pt-2 border-t border-gray-300 tracking-wide'>
-            <span className='nano caps text-gray-500'>classname:</span> peta
+        <p className='mt-8 pt-2 border-t border-gray-300 tracking-wide'>
+          <span className='nano caps text-gray-500'>classname:</span> lede
+        </p>
+      </div>
+      <div className='pt-12'>
+        <div className='narrative'>
+          <p>
+            On a sun-drenched street, the aroma of fresh pastries wafted from a bustling café,
+            merging with the city’s vibrant pulse. A breeze whispered through the trees, carrying
+            snippets of chatter and the distant hum of traffic.
+          </p>
+          <p>
+            In the heart of downtown, modern sculptures stood juxtaposed against historic buildings,
+            their silhouettes a testament to time’s passage. The evening sky, painted in hues of
+            orange and purple, draped over the skyline, its colors reflecting in the glass facades
+            of towering skyscrapers.
           </p>
         </div>
-        <div className='pt-12'>
-          <p className='tera'>Kingly sparrows quiz the evening wind</p>
-          <p className='mt-8 pt-2 border-t border-gray-300 tracking-wide'>
-            <span className='nano caps text-gray-500'>classname:</span> tera
-          </p>
-        </div>
-        <div className='pt-12'>
-          <p className='giga'>Brisk ferns envelop damp pathways</p>
-          <p className='mt-8 pt-2 border-t border-gray-300 tracking-wide'>
-            <span className='nano caps text-gray-500'>classname:</span> giga
-          </p>
-        </div>
-        <div className='pt-12'>
-          <p className='mega'>Gargantuan whelks explore neptunian depths</p>
-          <p className='mt-8 pt-2 border-t border-gray-300 tracking-wide'>
-            <span className='nano caps text-gray-500'>classname:</span> mega
-          </p>
-        </div>
-        <div className='pt-12'>
-          <p className='kilo'>Vibrant, quirky jackals roam zeniths, eclipsing moonlit fjords</p>
-          <p className='mt-8 pt-2 border-t border-gray-300 tracking-wide'>
-            <span className='nano caps text-gray-500'>classname:</span> kilo
-          </p>
-        </div>
+        <p className='mt-8 pt-2 border-t border-gray-300 tracking-wide'>
+          <span className='nano caps text-gray-500'>classname:</span> narrative
+        </p>
       </div>
     </main>
   );
