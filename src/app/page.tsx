1fffeebe8a0f6ad7bad4f7acbe53b66034b52752
Neil Baller <neil@gravybo.at>
2024-03-19 16:44:20 -0500

    Add nav links to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,32 +6,7 @@ export default function HomePage() {
       className='bg-cover bg-center bg-fixed min-h-screen'
       style={{ backgroundImage: 'url(gh-art/hp-bg.webp)', zIndex: -1 }}>
       <main className='text-white flex min-h-screen flex-col items-center justify-between'>
-        <div className='z-10 max-w-5xl w-full items-center justify-between text-sm lg:flex'>
-          <a
-            className='fixed left-0 top-0 flex w-full justify-center border-gray-500 hover:border-gray-100 pb-6 pt-8 backdrop-blur-sm hover:backdrop-blur-xl from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:p-4 shadow-2xl shadow-smoke-500'
-            href='/colors'>
-            Non Euclidian Color Selects &#9654;
-          </a>
-          <div className='fixed bottom-0 left-0 flex h-48 w-full items-end justify-center lg:static lg:h-auto lg:w-auto lg:bg-none'>
-            <a
-              className='pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0'
-              href='https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-              target='_blank'
-              rel='noopener noreferrer'>
-              Arcane{' '}
-              <Image
-                src='/vercel.svg'
-                alt='Vercel Logo'
-                className='invert'
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className='relative z-10 flex place-items-center'>
+        <div className='relative z-10 flex place-items-center py-4'>
           <Image
             className='relative'
             src='/gh-art/eldritchfoundation.webp'
@@ -43,6 +18,41 @@ export default function HomePage() {
         </div>
 
         <div className='mb-32 grid gap-4 text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left'>
+          <a
+            href='/colors'
+            className='group rounded-lg border border-gray-500 hover:border-gray-50 px-5 py-4 transition-colors backdrop-blur-sm hover:backdrop-blur-xl shadow-2xl shadow-smoke-500'
+            target='_blank'
+            rel='noopener noreferrer'>
+            <h2 className={`mb-3 text-2xl`}>
+              Colors{' '}
+              <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
+                &#9654;
+              </span>
+            </h2>
+            <p className={`m-0 max-w-[30ch] text-sm opacity-70`}>
+              Descend into the chromatic abyss, where hues unspeakable and tints unknowable converge
+              in a spectral dance.
+            </p>
+          </a>
+
+          <a
+            href='/typography'
+            className='group rounded-lg border border-gray-500 hover:border-gray-50 px-5 py-4 transition-colors backdrop-blur-sm hover:backdrop-blur-xl shadow-2xl shadow-smoke-500'
+            target='_blank'
+            rel='noopener noreferrer'>
+            <h2 className={`mb-3 text-2xl`}>
+              Typography{' '}
+              <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
+                &#9654;
+              </span>
+            </h2>
+            <p className={`m-0 max-w-[30ch] text-sm opacity-70`}>
+              Delve into a realm of forbidden scripts and unholy kernings, where typefaces writhe in
+              the shadows of ancient texts, each glyph poised to touch the very essence of unspoken
+              mysteries.
+            </p>
+          </a>
+
           <a
             href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
             className='group rounded-lg border border-gray-500 hover:border-gray-50 px-5 py-4 transition-colors backdrop-blur-sm hover:backdrop-blur-xl shadow-2xl shadow-smoke-500'
