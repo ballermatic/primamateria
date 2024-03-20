e16d2a0e54d48954bfb3f9407cd1503c6cf70640
Neil Baller <neil@gravybo.at>
2024-03-20 09:37:39 -0500

    Minor type tweaks

--- src/app/colors/page.tsx
+++ src/app/colors/page.tsx
@@ -5,19 +5,17 @@ export default function ColorsPage() {
   const { colors } = colorsData;
 
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between ~p-3/8 text-smoke-500'>
-      <h1 className='font-bold text-4xl'>Color Selects</h1>
-      <div className='container mx-auto py-4'>
-        {Object.entries(colors).map(([colorName, colorShades]) => (
-          <div key={colorName}>
-            <h2 className='text-xl font-bold mt-6 mb-2'>{colorName}</h2>
-            <ColorCard
-              colorName={colorName}
-              colorShades={colorShades}
-            />
-          </div>
-        ))}
-      </div>
+    <main className='container mx-auto flex min-h-screen flex-col ~p-3/8 text-smoke-500'>
+      <h1 className='giga text-gray-500 pb-12'>Color Selects</h1>
+      {Object.entries(colors).map(([colorName, colorShades]) => (
+        <div key={colorName}>
+          <h2 className='mt-8 mb-4 kilo'>{colorName}</h2>
+          <ColorCard
+            colorName={colorName}
+            colorShades={colorShades}
+          />
+        </div>
+      ))}
     </main>
   );
 }
