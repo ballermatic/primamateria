613c559c0e1c246cc3c3aab8c28358871891ef14
Neil Baller <neil@gravybo.at>
2024-04-02 18:29:33 -0500

    Fix invert property for logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,7 +1,6 @@
 import clsx from 'clsx';
 
 export default function Logo({
-  className,
   invert = false,
 }: React.ComponentPropsWithoutRef<'svg'> & { invert?: boolean }) {
   return (
@@ -11,10 +10,9 @@ export default function Logo({
       aria-hidden='true'
       viewBox='0 0 32 32'
       width={32}
-      height={32}
-      className={className}>
+      height={32}>
       <path
-        className={invert ? 'fill-white' : 'fill-smoke'}
+        className={clsx(invert ? 'fill-white' : 'fill-smoke')}
         d='M16 8V0C7.2 0 0 7.2 0 16h8c0-4.4 3.6-8 8-8zM24 16c0 4.4-3.6 8-8 8v8c8.8 0 16-7.2 16-16h-8zM8 16c0 4.4 3.6 8 8 8v-8H8zM16 8v8h8c0-4.4-3.6-8-8-8z'
       />
     </svg>
