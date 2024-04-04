a6b81d76f04252312b1fe0acd7fc8f053210acc5
Neil Baller <neil@gravybo.at>
2024-04-04 09:44:54 -0500

    Lucide react icons, more styling tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,41 @@
+import { ArrowRight, CircleAlert, ExternalLink } from 'lucide-react';
 import Link from 'next/link';
 
 import Mark from '@/components/logo/logo-mark';
 
+const todos = [
+  'Tune serif font weights - balance with serif',
+  'Improve mono font support - variable selection(s)',
+  'GSAP - mobile menu, then more',
+  'Analytics - Plausible vs Google',
+  'Open Graph image generation',
+  'Payload CMS - waiting on 3.x alpha',
+  'Layout components',
+  'Shiki for code blocks?',
+];
+
+function TodosList() {
+  return (
+    <ul>
+      {todos.map((todo, index) => (
+        <li
+          key={index}
+          className='mb-3'>
+          <CircleAlert
+            className='inline-block -mt-1 me-1 text-gray-400'
+            size={24}
+          />{' '}
+          {todo}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
 export default function HomePage() {
   return (
     <main className='container mx-auto flex flex-col items-center p-12 '>
-      <div className='flex max-w-lg flex-col items-center text-center'>
+      <div className='flex max-w-xl flex-col items-center text-center'>
         <Mark className='~my-12/24 size-64' />
         <h1 className='text-6xl font-serif tracking-tight mb-3'>Prima Materia</h1>
         <h2 className='kilo mb-6 text-gray-400'>
@@ -20,8 +50,8 @@ export default function HomePage() {
           <Link
             href='https://en.wikipedia.org/wiki/Prima_materia'
             target='_blank'
-            className='text-xs hover:text-blue-500'>
-            Wikipedia
+            className='inline-flex gap-1 items-center justify-center text-xs hover:text-blue-500'>
+            Wikipedia <ExternalLink size={11} />
           </Link>
         </div>
       </div>
@@ -33,7 +63,7 @@ export default function HomePage() {
           <h2 className='caps mb-3 font-medium group-hover:text-blue-500'>
             Colors{' '}
             <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-              &#9654;
+              <ArrowRight size={12} />
             </span>
           </h2>
           <p className='m-0 max-w-[30ch] group-hover:text-blue-500'>
@@ -47,7 +77,7 @@ export default function HomePage() {
           <h2 className='caps mb-3 font-medium group-hover:text-blue-500'>
             Typography{' '}
             <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-              &#9654;
+              <ArrowRight size={12} />
             </span>
           </h2>
           <p className='m-0 max-w-[30ch] group-hover:text-blue-500'>
@@ -61,7 +91,7 @@ export default function HomePage() {
           <h2 className='caps mb-3 font-medium group-hover:text-blue-500'>
             Sandbox{' '}
             <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-              &#9654;
+              <ArrowRight size={12} />
             </span>
           </h2>
           <p className='m-0 max-w-[30ch] group-hover:text-blue-500'>
@@ -72,8 +102,10 @@ export default function HomePage() {
 
       <div className='text-left w-full my-12'>
         <h2 className='text-lg font-medium mb-3'>
-          Specification -{' '}
-          <span className='text-gray-400 font-normal'>a basic yet opinionated Nextjs starter</span>
+          Specification{' '}
+          <span className='text-gray-400 font-normal'>
+            &hellip; yet another opinionated Nextjs starter
+          </span>
         </h2>
         <div className='flex flex-col md:flex-row bg-smoke text-white p-6 font-mono ~gap-4/8 text-sm'>
           <div className='md:w-1/2'>
@@ -92,19 +124,19 @@ export default function HomePage() {
             <h3 className='uppercase'>Extras</h3>
             <div className='mb-1'>------</div>
             <ul>
-              <li className='mb-2'>Tailwind Fluid</li>
-              <li className='mb-2'>More colors via tints.dev (OKLCH)</li>
+              <li className='mb-2'>Curated sans, serif, and mono webfonts</li>
               <li className='mb-2'>Support for variable font settings</li>
+              <li className='mb-2'>More colors via tints.dev (OKLCH)</li>
+              <li className='mb-2'>Tailwind Fluid</li>
               <li className='mb-2'>Tuned heading classes: kilo, giga, etc.</li>
-              <li className='mb-2'>Local and Google webfonts</li>
             </ul>
           </div>
         </div>
       </div>
 
       <div className='text-left w-full my-12'>
-        {' '}
-        <h2 className='text-lg font-medium mb-3'>To do</h2>
+        <h2 className='text-lg font-medium mb-3'>To do‘s</h2>
+        <TodosList />
       </div>
     </main>
   );
