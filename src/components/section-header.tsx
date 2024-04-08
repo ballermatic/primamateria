c9128045055b1292d9965c6b3d396e1af54cf06e
Neil Baller <neil@gravybo.at>
2024-04-08 17:20:59 -0500

    Spacing tweak

--- src/components/section-header.tsx
+++ src/components/section-header.tsx
@@ -1,5 +1,5 @@
 type SectionHeaderProps = { title: string };
 
 export default function SectionHeader({ title }: SectionHeaderProps) {
-  return <p className='mt-8 border-b-2 border-smoke pb-2 font-bold'>{title}</p>;
+  return <p className='mt-12 border-b-2 border-smoke pb-2 font-bold'>{title}</p>;
 }
