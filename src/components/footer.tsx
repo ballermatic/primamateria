467e7368d212680dc5fb548f0c3390ce90b9cfb8
Neil Baller <neil@gravybo.at>
2024-04-09 08:35:41 -0500

    Footer small screen improvements

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,9 +4,9 @@ import Logo from './logo';
 
 export default function Footer() {
   return (
-    <footer className='flex justify-between items-center mt-auto p-2 pb-3 pe-4 border-t border-smoke'>
+    <footer className='flex flex-col md:flex-row justify-between items-center mt-auto px-2 py-4 md:py-3 border-t border-smoke'>
       <Logo />
-      <div className='flex items-center nano gap-4 '>
+      <div className='flex items-center nano gap-4 mt-4 md:mt-0 md:me-2'>
         <Link
           href='/privacy'
           className='p-0.5 hover:text-blue-500 hover:border-blue-500 hover:border-b'>
@@ -17,7 +17,7 @@ export default function Footer() {
           className='p-0.5 hover:text-blue-500 hover:border-blue-500 hover:border-b'>
           Cookies
         </Link>
-        <p>©{new Date().getFullYear()} PrimaMateria. All rights reserved.</p>{' '}
+        <p>©{new Date().getFullYear()} PrimaMateria. All rights reserved.</p>
       </div>
     </footer>
   );
