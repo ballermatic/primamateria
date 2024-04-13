f2327c1785c1fb5f59bd7a242ba468b6a01a725f
Neil Baller <neil@gravybo.at>
2024-04-13 14:37:06 -0500

    Heading component for h1-h5

---
+++ src/components/heading.tsx
@@ -0,0 +1,82 @@
+import clsx from 'clsx';
+import { LinkIcon } from 'lucide-react';
+import Link from 'next/link';
+import React, { ReactNode } from 'react';
+
+type HeadingProps = {
+  level: 'h1' | 'h2' | 'h3' | 'h4' | 'h5';
+  preset?: 'exa' | 'peta' | 'tera' | 'giga' | 'mega' | 'kilo' | 'nano';
+  anchor?: boolean;
+  serif?: boolean;
+  className?: string;
+  children: ReactNode;
+};
+
+export default function Heading({
+  level,
+  preset,
+  anchor,
+  serif,
+  className,
+  children,
+  ...props
+}: HeadingProps) {
+  // Tune these based on font choices
+  const styles = {
+    exa: {
+      default: '~text-5xl/8xl text-balance font-[560] tracking-[-0.045em] leading-none mb-[0.5em]',
+      serif:
+        'font-serif ~text-5xl/8xl text-balance font-[560] tracking-[-0.045em] leading-none mb-[0.5em]',
+    },
+    peta: {
+      default: '~text-4xl/7xl text-balance font-[535] tracking-[-0.033em] leading-none mb-[0.5em]',
+      serif:
+        'font-serif ~text-4xl/7xl text-balance font-[535] tracking-[-0.033em] leading-none mb-[0.5em]',
+    },
+    tera: {
+      default: '~text-3xl/6xl text-balance font-[509] tracking-[-0.0275em] leading-none mb-[0.5em]',
+      serif: 'font-serif',
+    },
+    giga: {
+      default:
+        '~text-2xl/5xl text-balance font-[484] tracking-[-0.025em] leading-[1.125] mb-[0.5em]',
+      serif:
+        'font-serif ~text-3xl/6xl text-balance font-[509] tracking-[-0.0275em] leading-none mb-[0.5em]',
+    },
+    mega: {
+      default: '~text-xl/4xl text-balance font-[484] tracking-[-0.02em] leading-[1.125] mb-[0.5em]',
+      serif:
+        'font-serif ~text-xl/4xl text-balance font-[560] tracking-[-0.045em] leading-[1.125] mb-[0.5em]',
+    },
+    kilo: {
+      default: '~text-xl/3xl text-balance font-[410] tracking-[-0.02em] leading-[1.125] mb-[0.5em]',
+      serif:
+        'font-serif ~text-xl/3xl text-balance font-[410] tracking-[-0.02em] leading-[1.125] mb-[0.5em]',
+    },
+    nano: {
+      default: '~text-xxs/xs !important',
+      serif: 'font-serif ~text-xxs/xs !important',
+    },
+  };
+
+  // Allow serif to replace
+  const defaultClass = preset ? styles[preset].default : '';
+  const serifClass = preset && serif ? styles[preset].serif : '';
+  const headingClass = clsx(defaultClass, serifClass, className);
+
+  return (
+    <>
+      {anchor ? (
+        <Link
+          href={`#${children!.toString().toLowerCase().replace(/ /g, '-')}`}
+          className={clsx('group inline-flex items-center gap-1', headingClass)}
+          {...props}>
+          {children}
+          <LinkIcon className='size-[0.5em] opacity-0 group-hover:opacity-50' />
+        </Link>
+      ) : (
+        React.createElement(level, { className: headingClass, ...props }, children)
+      )}
+    </>
+  );
+}
